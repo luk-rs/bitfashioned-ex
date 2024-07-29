8a169e6eeb2db56e21912b177219cf4d117c819a
luk-rs <santos8@gmail.com>
2024-07-29 20:24:51 +0100

    refactor to tile component

--- src/app/containers/home/list-content/list-content.tsx
+++ src/app/containers/home/list-content/list-content.tsx
@@ -2,6 +2,14 @@ import { getFromIDB, IndexedImage } from '@/app/lib/indexed-db'
 import Image from 'next/image'
 import { useEffect, useState } from 'react'
 
+function TileComponent({ elem }: { elem: IndexedImage }) {
+  return (
+    <div className='w-44 h-44 relative m-5 bg-bf-signinbkg'>
+      <Image src={elem.ipfsUrl} alt={elem.description} fill className='rounded-md scale-90' />
+      <h3 className='absolute w-44 bottom-0 pb-5 left-0 text-center bg-bf-pagebkg opacity-55'>{elem.title}</h3>
+    </div>
+  )
+}
 export default function ListContent() {
   const [elems, setElems] = useState<IndexedImage[]>([])
 
@@ -15,15 +23,9 @@ export default function ListContent() {
 
   return (
     <div className='flex flex-wrap justify-center pt-10'>
-      {elems.map((elem, idx) => {
-        console.log(elem.ipfsUrl)
-        return (
-          <div key={`elem${idx}`} className='w-44 h-44 relative m-5 bg-bf-signinbkg'>
-            <Image src={elem.ipfsUrl} alt={elem.description} fill className='rounded-md scale-90' />
-            <h3 className='absolute w-44 bottom-0 pb-5 left-0 text-center bg-bf-pagebkg opacity-55'>{elem.title}</h3>
-          </div>
-        )
-      })}
+      {elems.map((elem, idx) => (
+        <TileComponent key={`elem${idx}`} elem={elem} />
+      ))}
     </div>
   )
 }
